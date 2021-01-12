a85fb6324a638221aed3b3fba31191f9c67b99e4
Aleš Hercok <alahercok@gmail.com>
2021-01-12 15:15:49 +0100

    Přidejte soubory projektu.

---
+++ Pexeso/Pexeso/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+using SQLite;
+
+namespace Pexeso
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //presmerovani na Easy, kde je lehka obtiznost
+        public void EasyMove(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Easy());
+        }
+
+        //presmerovani na Medium, kde je stredni obtiznost
+        public void MediumMove(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Medium());
+        }
+
+        //presmerovani na Hard, kde je tezka obtiznost
+        public void HardMove(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Hard());
+        }
+    }
+}
